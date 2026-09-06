c6d24af6acfb0949b522663451362cba4665389e
Grisma Bhandari <iamgrisma@gmail.com>
2026-09-06 14:31:55 -0700

    feat: include all storefront styles

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 import './store.css';
+import './store-extra.css';
 
 const root = document.getElementById('root');
 if (!root) throw new Error('BuyerNepal root element is missing');
