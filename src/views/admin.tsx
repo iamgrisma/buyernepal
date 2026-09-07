fadc11153b53dca20b5be60976a3c0b9bf06bbae
Grisma Bhandari <contact@grisma.com.np>
2026-09-07 15:04:57 +0545

    dfs

--- src/views/admin.tsx
+++ src/views/admin.tsx
@@ -243,7 +243,7 @@ export const AdminDashboardView: FC<{
         </aside>
 
         {/* Main Content Area */}
-        <main className="admin-main">
+        </main>
           {/* Top Info Bar */}
           <div className="admin-topbar-card">
             <div>
